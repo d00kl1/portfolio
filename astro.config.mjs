58c96197fa1d0d511ae0dd7d2d0ee7405ff41d6b
d00kl1 <harikrishnan_v@yahoo.com>
2026-09-15 08:04:35 -0700

    Remove cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare({
-    // Tells the adapter NOT to enable Cloudflare Images binding
-    // and let Sharp process static assets into /_astro/ at build time
-    imageService: 'passthrough',
-  }),
 });

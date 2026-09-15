7eef679b29fb7ee01384b37f0598299b8d36ff0e
d00kl1 <harikrishnan_v@yahoo.com>
2026-09-14 19:53:41 -0700

    Fix image URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://d00kl1.github.io',
-  base: '/',
+  // Force static image processing at build time for static hosts like Cloudflare Pages
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  }
 });

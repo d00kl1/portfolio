0eb09d8203456266c191630361b281319c66d1de
d00kl1 <harikrishnan_v@yahoo.com>
2026-09-14 20:00:21 -0700

    Make static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Force static image processing at build time for static hosts like Cloudflare Pages
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop'
-    }
-  }
+  output: 'static',  
 });

32b711d8c563f5c7b1cbc9c7b2ef05944b23fd6b
d00kl1 <harikrishnan_v@yahoo.com>
2026-09-15 07:44:12 -0700

    Add cloudflare back to enforce static images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-export default defineConfig({  
+export default defineConfig({
+  output: 'static',
+  adapter: cloudflare({
+    // Tells the adapter NOT to enable Cloudflare Images binding
+    // and let Sharp process static assets into /_astro/ at build time
+    imageService: 'passthrough',
+  }),
 });
